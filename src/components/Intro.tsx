95e4018146e7d19e3fa3203dcebbb7d6e57e6061
Rakshit <rakshit087@gmail.com>
2022-08-27 22:31:35 +0530

    Added gradient 🥹

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,6 @@
 export const Intro = () => {
   return (
-    <div id="intro" className="flex items-center justify-center h-sceen">
+    <div id="intro" className="flex items-center justify-center h-screen">
       <div>
         <p>Hi, I am</p>
         <p>Rakshit Arora.</p>
