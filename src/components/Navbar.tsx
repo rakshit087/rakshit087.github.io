14c027421052a1f0ad399f506eadfec7550c8c73
Rakshit <rakshit087@gmail.com>
2022-08-28 00:47:16 +0530

    Added Icons & Blur to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
+import { IoMenu, IoUnlink } from 'react-icons/io5';
+
 export const Navbar = () => {
   return (
     <>
-      <div className="fixed z-50 flex justify-between w-5/6 px-4 py-1 my-6 -translate-x-1/2 border-2 border-gray-500 border-solid rounded-full left-1/2">
-        <button />
-        <button />
+      <div className="fixed z-50 flex justify-between w-5/6 px-4 py-2 -translate-x-1/2 border-2 border-gray-700 rounded-full top-8 left-1/2 backdrop-blur-md">
+        <button className="p-1 ">
+          <IoMenu className="text-xl" />
+        </button>
+        <button className="p-1 bg-pink-100 border-2 border-gray-700 rounded-full">
+          <IoUnlink className="text-xl" />
+        </button>
       </div>
     </>
   );
