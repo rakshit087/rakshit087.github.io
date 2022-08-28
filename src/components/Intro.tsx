54d844acc7d65199e41f5d82cf7b4287d22473e7
Rakshit <rakshit087@gmail.com>
2022-08-28 21:11:31 +0530

    Completed Intro Element

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,18 @@
+import { motion } from 'framer-motion';
+import { CgScrollV } from 'react-icons/cg';
 export const Intro = () => {
   return (
-    <div id="intro" className="flex items-center justify-center h-screen">
+    <div id="intro" className="flex flex-col items-center justify-center h-screen">
       <div>
-        <p>Hi, I am</p>
-        <p>Rakshit Arora.</p>
+        <p className="text-xl tracking-wider">Hi, I am</p>
+        <p className="text-3xl tracking-wider">
+          Rakshit <strong>Arora</strong>,
+        </p>
+        <p className="mt-2 text-lg">and I love building cool stuff 🛠</p>
       </div>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+        <CgScrollV className="fixed bottom-5" />
+      </motion.div>
     </div>
   );
 };
