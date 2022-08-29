19c6fd7ad05ebf2cb958cc80219ab636cfa935e1
Rakshit <rakshit087@gmail.com>
2022-08-29 12:38:15 +0530

    Started working on Projects component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,7 @@
+export const Projects = () => {
+  return (
+    <div className="flex justify-center min-h-screen py-24 snap-mandatory snap-start">
+      <p className="text-3xl">Projects</p>
+    </div>
+  );
+};
