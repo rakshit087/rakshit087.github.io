ea5efb3641c893a869e9314890057678729e8597
Rakshit <rakshit087@gmail.com>
2022-08-30 08:01:36 +0530

    Removed scrollbar

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,5 +1,3 @@
-import { motion } from 'framer-motion';
-import { CgScrollV } from 'react-icons/cg';
 export const Intro = () => {
   return (
     <div id="intro" className="flex flex-col items-center justify-center h-screen snap-start snap-mandatory">
@@ -10,9 +8,6 @@ export const Intro = () => {
         </p>
         <p className="mt-2 text-lg">and I love building cool stuff 🛠</p>
       </div>
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-        <CgScrollV className="absolute bottom-5" />
-      </motion.div>
     </div>
   );
 };
