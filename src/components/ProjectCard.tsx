a75b7f7ff0fdaf113637bd2c5d547d43d7abf4c0
Rakshit <rakshit087@gmail.com>
2022-08-30 23:37:45 +0530

    Added projects section

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,12 +1,29 @@
 export const ProjectCard = () => {
   return (
-    <div className="w-full mb-8 bg-white border-2 border-gray-700 border-solid rounded-xl h-52">
-      <div
-        className="relative right-0 w-5 h-5 -translate-y-1/2 bg-gray-700 border-2 border-gray-400 border-solid rounded-full top-1/2"
-        style={{
-          right: '29px',
-        }}
-      ></div>
-    </div>
+    <>
+      <div className="px-2 py-1 mb-2 border border-gray-700 border-solid rounded-full bg-pink-50 w-fit">
+        <p className="text-xs">Aug '22 to Nov '22</p>
+      </div>
+      <div className="w-full mb-8 bg-white border-2 border-gray-700 border-solid rounded-xl">
+        <div
+          className="relative right-0 w-5 h-5 -translate-y-1/2 bg-gray-700 border-2 border-gray-400 border-solid rounded-full top-1/2"
+          style={{
+            right: '29px',
+          }}
+        ></div>
+        <div className="px-4">
+          <h2 className="mb-4 text-2xl">Kolumn</h2>
+          <p className="mb-4">
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in
+            some form, by injected humour, or randomised words which don't look even slightly believable..
+          </p>
+          <div className="flex mb-4">
+            <p className="mr-4 text-gray-700 underline">Live</p>
+            <p className="mr-4 text-gray-700 underline">Gallery</p>
+            <p className="mr-4 text-gray-700 underline">Code</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@ import 'react-vertical-timeline-component/style.min.css';
 
 export const Projects = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen px-12 py-24 snap-mandatory snap-start add-bg scroll-smooth">
+    <div className="flex flex-col items-center min-h-screen px-8 py-24 snap-mandatory snap-start add-bg scroll-smooth">
       <p className="mb-8 text-3xl">Projects</p>
       <div className="flex w-full">
         <div className="mr-4 border border-gray-400 border-2-2"></div>
