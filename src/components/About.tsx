b26aba4c7f6dcda84f89f33dcda4c2b3da3c203d
Rakshit <rakshit087@gmail.com>
2022-08-31 23:54:58 +0530

    Added shadows and a resume button

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,23 +13,26 @@ export const About = () => {
       <p className="mb-4 text-3xl font-bold">About</p>
       <p className="mb-16 text-lg text-center">
         As a builder, I am passionate about bringing ideas to life and writing awesome code that delivers impact and
-        translates to amazing user experience ✨.
+        translates to amazing user experience.
       </p>
       <p className="text-xl font-semibold">Other than Dev I ❤️</p>
       <div className="flex justify-between w-5/6 mt-4">
-        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-yellow-50">
+        <div className="flex items-center justify-center w-10 h-10 rounded-full shadow-md bg-yellow-50">
           <Image src="/images/naruto.svg" alt="anime" width={32} height={32} />
         </div>
-        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-purple-50">
+        <div className="flex items-center justify-center w-10 h-10 rounded-full shadow-md bg-purple-50">
           <Image src="/images/league.svg" alt="anime" width={32} height={32} />
         </div>
-        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-cyan-50">
+        <div className="flex items-center justify-center w-10 h-10 rounded-full shadow-md bg-cyan-50">
           <IoAirplane fontSize={24} />
         </div>
-        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-pink-50">
+        <div className="flex items-center justify-center w-10 h-10 rounded-full shadow-md bg-pink-50">
           <Image src="/images/music.svg" alt="anime" width={32} height={32} />
         </div>
       </div>
+      <button className="relative px-8 py-1 bg-white border-2 border-gray-700 rounded-full shadow-md top-16">
+        Resume
+      </button>
     </div>
   );
 };
