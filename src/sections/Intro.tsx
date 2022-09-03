e1c9b8243b42adf9bcd294c46065cba03f50d1ba
Rakshit <rakshit087@gmail.com>
2022-09-03 12:55:42 +0530

    Made Intro for lg screens

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -2,11 +2,11 @@ export const Intro = () => {
   return (
     <section id="intro" className="flex flex-col items-center justify-center fill-screen snap-start add-bg">
       <div>
-        <p className="text-xl tracking-wider md:text-2xl">Hi, I am</p>
-        <p className="text-3xl tracking-wider md:text-4xl">
+        <p className="text-xl tracking-wider md:text-2xl lg:text-3xl lg:mb-1">Hi, I am</p>
+        <p className="text-3xl tracking-wider md:text-4xl lg:text-5xl lg:mb-1">
           Rakshit <strong>Arora</strong>,
         </p>
-        <p className="mt-2 text-lg md:text-xl">and I love building cool stuff 🛠</p>
+        <p className="mt-2 text-lg md:text-xl lg:text-2xl ">and I love building cool stuff 🛠</p>
       </div>
     </section>
   );
