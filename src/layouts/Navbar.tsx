75e9331ec03db2429fc1b1394d4e3497c06f7572
Rakshit <rakshit087@gmail.com>
2022-09-03 12:28:50 +0530

    Created Seperate component for NavMenu

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react';
-import { IoMenu } from 'react-icons/io5';
-import { motion } from 'framer-motion';
-
-const variants = {
-  open: { opacity: 1 },
-  closed: { opacity: 0 },
-};
+import { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+import { NavMenuMobile } from "../components/NavMenuMobile";
 
 export const Navbar = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -22,56 +17,7 @@ export const Navbar = () => {
           <IoMenu className="text-xl" />
         </button>
       </nav>
-      <motion.div
-        className={open ? 'absolute fill-screen z-20' : 'hidden'}
-        animate={open ? 'open' : 'closed'}
-        variants={variants}
-        onClick={() => {
-          setOpen(false);
-        }}
-      >
-        <div className="z-30 py-20 bg-white shadow-lg px-14">
-          <p className="mb-4 text-2xl font-bold">Jump To</p>
-          <div className="flex flex-col">
-            <a
-              href="#intro"
-              className="mb-2 text-xl"
-              onClick={() => {
-                setOpen(false);
-              }}
-            >
-              Intro
-            </a>
-            <a
-              href="#about"
-              className="mb-2 text-xl"
-              onClick={() => {
-                setOpen(false);
-              }}
-            >
-              About Me
-            </a>
-            <a
-              href="#projects"
-              className="mb-2 text-xl"
-              onClick={() => {
-                setOpen(false);
-              }}
-            >
-              My Projects
-            </a>
-            <a
-              href="#connect"
-              className="mb-2 text-xl"
-              onClick={() => {
-                setOpen(false);
-              }}
-            >
-              Contact Me
-            </a>
-          </div>
-        </div>
-      </motion.div>
+      <NavMenuMobile open={open} setOpen={setOpen} />
     </>
   );
 };
