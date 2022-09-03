dc768d8efdd182d057e56a29c3496bfbc554673b
Rakshit <rakshit087@gmail.com>
2022-09-03 13:51:38 +0530

    Added buttons in NavMenu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,28 +1,19 @@
+const buttonClasses =
+  "px-2 py-1 mr-4 text-lg border-2 border-gray-700 border-solid rounded-full bg-gradient-to-br from-pink-50 to-cyan-50";
+
 export const NavMenu = () => {
   return (
     <div className="hidden lg:flex">
-      <a
-        href="#intro"
-        className="px-2 py-1 mr-4 text-lg border-2 border-gray-700 border-solid rounded-full bg-gradient-to-br from-pink-100 to-cyan-100"
-      >
+      <a href="#intro" className={buttonClasses}>
         Intro
       </a>
-      <a
-        href="#about"
-        className="px-2 py-1 mr-4 text-lg border-2 border-gray-700 border-solid rounded-full bg-gradient-to-r from-pink-100 to-cyan-100"
-      >
+      <a href="#about" className={buttonClasses}>
         About Me
       </a>
-      <a
-        href="#projects"
-        className="px-2 py-1 mr-4 text-lg border-2 border-gray-700 border-solid rounded-full bg-gradient-to-bl from-pink-100 to-cyan-100"
-      >
+      <a href="#projects" className={buttonClasses}>
         My Projects
       </a>
-      <a
-        href="#connect"
-        className="px-2 py-1 mr-4 text-lg border-2 border-gray-700 border-solid rounded-full bg-gradient-to-r from-pink-100 to-cyan-100"
-      >
+      <a href="#connect" className={buttonClasses}>
         Contact Me
       </a>
     </div>
