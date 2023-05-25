ac5b6875b159862fe5ba2e2154f364e022d0538d
Rakshit Arora <rakshit087@gmail.com>
2023-05-25 11:34:07 +0530

    setup github actions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const githubPages = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (githubPages) {
+  assetPrefix = '/portfolio/'
+  basePath = '/portfolio'
+}
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix,
+  basePath,
 };
 
 module.exports = nextConfig;
