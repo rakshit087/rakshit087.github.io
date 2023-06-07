140a2e8e3051e47cf301a967e912d8ad4eaab68e
Rakshit <rakshit087@gmail.com>
2023-06-07 11:15:25 +0530

    made the Projects section responsive

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -16,10 +16,10 @@ export const ProjectCard = ({ title, desc, date, gallery, code, live }: project)
       <div className="py-1 px-2 mb-2 bg-pink-50 rounded-full border border-gray-700 border-solid w-fit">
         <p className="text-xs md:text-sm">{date}</p>
       </div>
-      <div className="flex-1 py-6 px-4 mb-8 bg-white rounded-xl border-2 border-gray-700 border-solid cursor-pointer md:mb-10">
+      <div className="flex flex-col flex-grow justify-between py-6 px-4 mb-8 bg-white rounded-xl border-2 border-gray-700 border-solid cursor-pointer md:mb-10">
         <h2 className="mb-4 text-2xl md:mb-6 md:text-3xl">{title}</h2>
-        <p className="mb-4 md:mb-6 md:text-lg">{desc}</p>
-        <div className="flex mb-4">
+        <p className="mb-4 md:mb-6">{desc}</p>
+        <div className="flex">
           {live && (
             <a href={live} target="_blank" rel="noreferrer" className="mr-4 text-lg text-gray-700 md:mr-6 md:text-xl">
               <MdLink />
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,8 +1,8 @@
 import { ProjectCard } from "../components/ProjectCard";
 import data from "../data/projects.json";
-import { useEffect, useState, useRef } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 type projects = {
   title: string;
@@ -14,43 +14,36 @@ type projects = {
   tags: string[];
 }[];
 
-const tags = ["all", "automation", "full stack", "system", "web 3"];
-
 export const Projects = () => {
- const projects: projects = data;
+  const projects: projects = data;
   const swiperElRef = useRef(null);
 
   return (
     <section
       id="projects"
-      className="flex flex-col items-center h-screen min-h-screen px-8 py-24 add-bg md:px-32 lg:py-32 snap-start snap-always"
+      className="flex flex-col justify-center items-center py-24 px-8 h-screen min-h-screen lg:py-32 lg:px-32 add-bg snap-start snap-always"
     >
-      <div className="flex flex-col items-center justify-center w-full">
-        <p className="mb-6 text-3xl font-bold md:text-4xl">Personal Projects</p>
-        <Swiper
-          ref={swiperElRef}
-          spaceBetween={50}
-          slidesPerView={1}
-          breakpoints={{
-            640: {
-              slidesPerView: 2,
-            },
-            768: {
-              slidesPerView: 3,
-            },
-            1024: {
-              slidesPerView: 3,
-            },
-          }}
-          className="w-full"
-        >
-          {projects.map((project, index) => (
-            <SwiperSlide key={index}>
-              <ProjectCard {...project} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <p className="mb-12 text-3xl font-bold md:text-4xl">Personal Projects</p>
+      <Swiper
+        ref={swiperElRef}
+        spaceBetween={50}
+        slidesPerView={1}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }}
+        className="w-full"
+      >
+        {projects.map((project, index) => (
+          <SwiperSlide key={index}>
+            <ProjectCard {...project} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };
